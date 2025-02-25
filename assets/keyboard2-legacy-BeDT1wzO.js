60f79662172c4824af1cf01a27aac156440dada3
cacaii03 <daparmaricar3@gmail.com>
2025-02-25 11:28:28 +0800

    Updates

--- assets/keyboard2-legacy-BeDT1wzO.js
+++ assets/keyboard2-legacy-BeDT1wzO.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CHnOUq_7.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-CCnvimkJ.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
