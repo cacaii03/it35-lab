b1f3cbe4438c11036f5a36fc91f925171b0c9da3
maricar <daparmaricar3@gmail.com>
2025-03-07 14:16:33 +0800

    Added new features in feed.tsx and login.tsx page

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -1,24 +1,49 @@
+import React from 'react'; 
 import { 
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar 
-  } from '@ionic/react';
-  const Feed: React.FC = () => {
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Feed</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
+  IonButton, 
+  IonCard, 
+  IonCardContent, 
+  IonCardHeader, 
+  IonCardSubtitle, 
+  IonCardTitle, 
+  IonButtons,
+  IonContent, 
+  IonHeader, 
+  IonMenuButton, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar 
+} from '@ionic/react';
+
+const Example: React.FC = () => {
+  return (
+    <IonCard>
+      <IonCardHeader>
+        <IonCardTitle>Welcome!</IonCardTitle>
+        <IonCardSubtitle>Let's go.</IonCardSubtitle>
+      </IonCardHeader>
+      <IonCardContent>
+       Own study body space site.
+      </IonCardContent>
+
+      <IonButton fill="clear">Action 1</IonButton>
+      <IonButton fill="clear">Action 2</IonButton>
+    </IonCard>
+  );
+};
+
+const Feed: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot='start'>
+            <IonMenuButton></IonMenuButton>
+          </IonButtons>
+          <IonTitle>Feed</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
         <div
           style={{
             display: 'flex',
@@ -27,10 +52,11 @@ import {
             height: '100%',
           }}
         >
-          Feed
+          <Example />
         </div>
-        </IonContent>
-      </IonPage>
-    );
-  };
-  export default Feed;
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Feed;
