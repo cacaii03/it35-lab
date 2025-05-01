46fef5a84e6620945e085d920f08df7599945671
cacaii03 <daparmaricar3@gmail.com>
2025-05-01 19:23:32 +0800

    set up auth path modify

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
- 
- const supabaseUrl = 'https://rgmhmrypftckljyyrxcw.supabase.co';
- const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJnbWhtcnlwZnRja2xqeXlyeGN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI4NTk5MDcsImV4cCI6MjA1ODQzNTkwN30.-fdiRJECXhhs3mV6Dhxxua2iI1yzznk641d_HYQHga4';
- 
- export const supabase = createClient(supabaseUrl, supabaseKey);
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
