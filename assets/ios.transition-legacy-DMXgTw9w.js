c194ae6d2cc64a36d158cf9510cfe16394d6988a
cacaii03 <daparmaricar3@gmail.com>
2025-05-01 20:13:56 +0800

    Updates

--- assets/ios.transition-legacy-DMXgTw9w.js
+++ assets/ios.transition-legacy-DMXgTw9w.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DfPTfmAQ.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-DmCJyq3E.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
